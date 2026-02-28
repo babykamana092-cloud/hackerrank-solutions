0c132b01c9ac826443863e33e7084a3d8f6f04b7
babykamana092-cloud <babykamana092@gmail.com>
2026-02-28 12:34:29 +0530

    Create Sum of Digits of a Five Digit Number.c

---
+++ Sum of Digits of a Five Digit Number.c
@@ -0,0 +1,12 @@
+int main() {
+    int n, sum = 0;
+    scanf("%d", &n);
+
+    while (n > 0) {
+        sum += n % 10;  // get last digit
+        n /= 10;       // remove last digit
+    }
+
+    printf("%d", sum);
+    return 0;
+}
