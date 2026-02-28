c1f5343462487face1e882a52c1f320b7cf8d55d
babykamana092-cloud <babykamana092@gmail.com>
2026-02-28 12:36:19 +0530

    Create Bitwise Operators.C

---
+++ Bitwise Operators.C
@@ -0,0 +1,18 @@
+void calculate_the_maximum(int n, int k) {
+    int max_and = 0;
+    int max_or = 0;
+    int max_xor = 0;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = i + 1; j <= n; j++) {
+
+            if ((i & j) < k && (i & j) > max_and)
+                max_and = i & j;
+
+            if ((i | j) < k && (i | j) > max_or)
+                max_or = i | j;
+
+            if ((i ^ j) < k && (i ^ j) > max_xor)
+                max_xor = i ^ j;
+        }
+    }
